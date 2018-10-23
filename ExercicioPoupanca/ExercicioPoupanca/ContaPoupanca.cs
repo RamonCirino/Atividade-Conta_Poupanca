9d4efc509f6fae35fb227b4490dae9ada648dec4
Ramon Cirino dos Santos <42974954+RamonCirino@users.noreply.github.com>
2018-10-22 21:48:29 -0300

    Add files via upload

---
+++ ExercicioPoupanca/ExercicioPoupanca/ContaPoupanca.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPoupanca
+{
+    class ContaPoupanca
+    {
+        
+        
+            // Atributos da Classe ContaPoupanca
+
+            private static double TaxaDeJurosAnual;
+            private double saldoPoupanca;
+
+            // Construtor da Classe
+
+            public ContaPoupanca()
+            {
+                TaxaDeJurosAnual = 0;
+                saldoPoupanca = 0;
+            }
+
+            // Assinatura da Classe
+
+            public ContaPoupanca(double saldoPoupanca)
+            {
+                this.saldoPoupanca = saldoPoupanca;
+                TaxaDeJurosAnual = 0;
+            }
+
+            // Método CalcularJuromensal
+
+            public double CalcularJuromensal()
+            {
+                double juroMensal = ((saldoPoupanca * TaxaDeJurosAnual) / 12) + saldoPoupanca;
+                return juroMensal;
+            }
+
+            // Método para Alterar a taxaDeJurosAnual
+
+            public static void AlterarTaxaDeJuros(double taxaDeJuros)
+            {
+                TaxaDeJurosAnual = taxaDeJuros;
+            }
+
+            // Configurar saldoPoupanca
+
+            public double saldo
+            {
+                get
+                {
+                    return saldoPoupanca;
+                }
+                set
+                {
+                    saldoPoupanca = value;
+                }
+            }
+
+            // Configurar taxaDeJurosAnual
+
+            public static double taxaDeJuros
+            {
+                get
+                {
+                    return taxaDeJuros;
+                }
+                set
+                {
+                    taxaDeJuros = value;
+                }
+            }
+        }
+    }
+
+
---
+++ ExercicioPoupanca/ExercicioPoupanca/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando dois objetos ContaPoupanca
+
+            ContaPoupanca Poupador1 = new ContaPoupanca(2000);
+            ContaPoupanca Poupador2 = new ContaPoupanca(3000);
+
+            Console.WriteLine("Dados Poupador 1, saldo = 2000 mil reais");
+            Console.WriteLine("      Poupador 2, saldo = 3000 mil reais \n");
+
+            Console.WriteLine("Considere a taxa de Juros à 4%: \n");
+
+            ContaPoupanca.AlterarTaxaDeJuros(0.04);
+
+            Console.WriteLine("Com essa taxa de juros o novo saldo será de:  \n");
+            Console.WriteLine("Poupador 1: " + Poupador1.CalcularJuromensal());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Poupador 2: " + Poupador2.CalcularJuromensal());
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Saldo atualizado a 4%:\n\nPoupador 1, saldo = 2006,66666666667 mil reais");
+            Console.WriteLine("Poupador 2, saldo = 3010 mil reais \n");
+
+            Console.WriteLine("Caso a taxa de Juros seja de 5%: \n");
+
+            ContaPoupanca.AlterarTaxaDeJuros(0.05);
+
+            Console.WriteLine("o novo saldo será de:  \n");
+            Console.WriteLine("Poupador 1: " + Poupador1.CalcularJuromensal());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Poupador 2: " + Poupador2.CalcularJuromensal());
+
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Pressione a tecla ENTER para sair . . .");
+
+            Console.ReadKey();
+
+        }
+    }
+}
+        
+    
+
